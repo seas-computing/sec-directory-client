908fa5268f24dcf870cb6b4762124e99b425952c
Jonathan Seitz <jseitz@seas.harvard.edu>
2021-08-11 15:53:08 -0400

    Create Header component
    This is essentially a static component that replicates the header from
    the mockup in react, using some slight variations on the existing css to
    simplify the layouts. Since we're going to display this on fixed-size
    monitors (1920x1080), I've opted to use hard-coded height/widths in a
    number of places and left out some of the @media queries that were used
    initially.

---
+++ src/Header/index.tsx
@@ -0,0 +1,36 @@
+import './Header.css';
+import headerLogo from './header_seas_logo.png';
+import bannerBackground from './banner.jpg';
+
+interface HeaderProps {
+  /** The application title that will be displayed **/
+  children: string;
+}
+
+/**
+ * A static header componet that renders the school logo and banner image, along
+ * with the title of the app
+ */
+
+
+const Header = ({ children }: HeaderProps) => (
+  <div className="app-header">
+    <div className="app-header--logo-wrapper">
+      <img 
+        className="app-header--logo" 
+        src={headerLogo} 
+        alt="Harvard John A. Paulson School of Engineering and Applied Sciences"
+      />
+    </div>
+    <div className="app-header--title-wrapper">
+      <h1 className="app-header--title">{children}</h1>
+      <img
+        className="app-header--title-image"
+        src={bannerBackground}
+        alt="A computer screen rendering a 3D simulation alongside commands in a terminal"
+      />
+    </div>
+  </div>
+);
+
+export default Header;
