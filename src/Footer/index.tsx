cc9cf136891140df2d98941d09b9a8dd1a24b6e9
Jonathan Seitz <jseitz@seas.harvard.edu>
2021-08-12 16:32:39 -0400

    Add tests for date generation
    In the process, realized that I needed to move the instantiation to
    inside the render function to get it to update as expected.

--- src/Footer/index.tsx
+++ src/Footer/index.tsx
@@ -1,19 +1,20 @@
 import './Footer.css';
 import footerLogo from './footer.png';
 
-const currentYear = new Date().getUTCFullYear();
-
-const Footer = () => (
-  <div className="app-footer">
-    <img 
-      className="app-footer--logo" 
-      src={footerLogo} 
-      alt="Harvard John A. Paulson School of Engineering and Applied Sciences"
-    />
-      <p className="app-footer--copyright">
-        {`© ${currentYear} President and Fellows of Harvard College`}
-      </p>
-  </div>
-);
+const Footer = () => {
+  const currentYear = new Date().getUTCFullYear();
+  return (
+    <div className="app-footer">
+      <img
+        className="app-footer--logo"
+        src={footerLogo}
+        alt="Harvard John A. Paulson School of Engineering and Applied Sciences"
+      />
+        <p className="app-footer--copyright">
+          {`© ${currentYear} President and Fellows of Harvard College`}
+        </p>
+    </div>
+  )
+};
 
 export default Footer;
