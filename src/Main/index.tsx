3141300dc1ca91914f73c8142c615e4be74fd2b5
Jonathan Seitz <jseitz@seas.harvard.edu>
2021-08-13 11:45:21 -0400

    Create Main component

---
+++ src/Main/index.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+import './Main.css';
+
+interface MainProps { 
+  /** 
+   * The content that should be displayed inside the Main section of the page
+   */
+  children: ReactNode
+}
+
+const Main = ({children}: MainProps) => (
+ <main className="app-main">
+  {children}
+ </main>
+);
+
+export default Main;
