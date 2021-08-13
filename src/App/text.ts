20d878e6ad7e58eb7fa2a58d5a4862cff635b5c9
Jonathan Seitz <jseitz@seas.harvard.edu>
2021-08-13 11:46:20 -0400

    Add Welcome message to App component
    This moves all the App components to a new App/ subdirectory and adds
    the Main and Welcome components between the Header and Footer. It also
    adds a new State value for "currentView" to use to change what data is
    being displayed. In the future, we can add SEARCH, RESULTS, etc. to show
    other views.
    
    Hard coded text has also been split out into a separate file, to make
    editing updating text across the app easier.

---
+++ src/App/text.ts
@@ -0,0 +1,19 @@
+/**
+ * Collection of hard-coded text strings that appear in the App
+ */
+
+/**
+ * The name displayed at the top of the app
+ */
+export const appName = 'Allston Campus Directory';
+
+/**
+ * The text that appears in the sub-heading on the initial welcome screen
+ */
+export const welcomeBannerText = 'Welcome to Allston Campus';
+
+/**
+ * The instructions for how to use the system which will be displayed on the
+ * initial welcome screen
+ */
+export const welcomeInstructions = 'Touch anywhere on this screen to search for SEAS offices and services, faculty and staff in the Allston Campus';
