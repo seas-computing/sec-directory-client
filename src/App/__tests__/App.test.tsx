edd8fa1d7189cff841e255a85234b07c4238056c
Jonathan Seitz <jseitz@seas.harvard.edu>
2021-08-17 13:07:54 -0400

    Test reloading functionality
    It was a little tricky to stub out the window.location.reload function,
    but this should be enough testing to confirm that the core functionality
    works.

--- src/App/__tests__/App.test.tsx
+++ src/App/__tests__/App.test.tsx
@@ -1,6 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import App from '..';
 import {
+  RELOAD_INTERVAL_MS,
   WELCOME_BANNER,
   WELCOME_INSTRUCTIONS
 } from '../../const';
@@ -19,6 +20,44 @@ describe('App', function () {
         expect(screen.getByText(WELCOME_INSTRUCTIONS))
           .toBeInTheDocument();
       });
+    });
+  });
+  describe('Reloading Timeout', function () {
+    const { location: originalLocation } = window;
+    // replacing window.location as described in https://remarkablemark.org/blog/2018/11/17/mock-window-location/
+    beforeAll(function () {
+      // We can ignore this error because we're replacing location with mocked
+      // implementation immediately
+      // @ts-expect-error
+      delete window.location;
+      window.location = { ...originalLocation, reload: jest.fn() };
+    });
+    afterAll(function () {
+      window.location = originalLocation;
+    });
+    beforeEach(function () {
+      jest.useFakeTimers('modern')
+      render(<App />);
+    });
+    afterEach(function () {
+      jest.resetAllMocks();
+    });
+    it('Should reload the page if interval elapses without activity', function () {
+      jest.advanceTimersByTime(RELOAD_INTERVAL_MS * 1.25 );
+      expect(window.location.reload).toHaveBeenCalled();
+    });
+    it('Should restart the interval after any activity', function () {
+      //Get halfway through the timer
+      jest.advanceTimersByTime(RELOAD_INTERVAL_MS * 0.5);
+      //Open the keyboard
+      fireEvent.click(document.body);
+      // Go 75% through the new timer, so we should be at total 125% of our
+      // elapsed time
+      jest.advanceTimersByTime(RELOAD_INTERVAL_MS * 0.75);
+      expect(window.location.reload).not.toHaveBeenCalled();
+      // Now let the full timer time elapse
+      jest.advanceTimersByTime(RELOAD_INTERVAL_MS);
+      expect(window.location.reload).toHaveBeenCalled();
     });
   });
 });
