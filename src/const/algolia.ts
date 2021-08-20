e10d7e1279703ac2800a6382567c4abd40e8a384
Jonathan Seitz <jseitz@seas.harvard.edu>
2021-08-20 11:32:06 -0400

    Create algolia.ts to read from env
    The Create React App configs allow you to [substitute environment
    variables] at build/run time, so we set the Algolia configure values in
    a .env file, instead of needing to manually create and update a plain ts
    file.
    
    Although those docs recommend committing the .env files, we're not going
    to do that and will instead set those values as part of our GitHub
    Actions environment so they'll be available when building on CI.
    
    [1]: https://create-react-app.dev/docs/adding-custom-environment-variables

---
+++ src/const/algolia.ts
@@ -0,0 +1,3 @@
+export const ALGOLIA_API_KEY = process.env.REACT_APP_ALGOLIA_API_KEY;
+export const ALGOLIA_APP_ID = process.env.REACT_APP_ALGOLIA_APP_ID;
+export const ALGOLIA_INDEX = process.env.REACT_APP_ALGOLIA_INDEX;
