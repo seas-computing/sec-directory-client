3d750b8f5f5d96e9c8e4a1d75a9d895e0e1ba8d8
Jonathan Seitz <jseitz@seas.harvard.edu>
2021-08-31 15:34:38 -0400

    Tweak language on landing page
    This just makes two small tweaks requested by the communications team.

--- src/const/text.ts
+++ src/const/text.ts
@@ -10,10 +10,10 @@ export const APP_NAME = 'Allston Campus Directory';
 /**
  * The text that appears in the sub-heading on the initial welcome screen
  */
-export const WELCOME_BANNER = 'Welcome to Allston Campus';
+export const WELCOME_BANNER = 'Welcome to the Allston Campus';
 
 /**
  * The instructions for how to use the system which will be displayed on the
  * initial welcome screen
  */
-export const WELCOME_INSTRUCTIONS = 'Touch anywhere on this screen to search for SEAS offices, faculty and staff in the Allston Campus';
+export const WELCOME_INSTRUCTIONS = 'Touch anywhere on this screen to search for SEAS offices, faculty and staff on the Allston Campus';
